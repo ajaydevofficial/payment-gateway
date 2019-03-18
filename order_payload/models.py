e029b27c4c2e7d9a6b77f00125e7411ba4162495
unknown <ajaydevofficial@gmail.com>
2019-03-18 18:56:07 +0530

    Order_ID table

--- order_payload/models.py
+++ order_payload/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class order_id(models.Model):
+    order_id = models.IntegerField(max_digits=100)
+
+    def __str__(self):
+        return 'ORDER_ID'
--- processor/views.py
+++ processor/views.py
@@ -3,11 +3,18 @@ from Document.models import document
 
 def home_page(request):
     context = {}
+    #code for downloading Integration Docs File from Template
     try:
             doc = document.objects.get(name="Integration Docs")
             context = {'file':doc}
-
-            return render(request,"index.html",context)
     except:
-        print("File not in Database")
+        pass
+    #order-paylod variables which are mandatory
+    MID = 'KLUIOi74399454829212' #use your test or original MID from paytm buisness account
+    ORDER_ID = 0
+
+
+    if request.method=='POST':
+        pass
+
     return render(request,"index.html",context)
