b26a02439dc667c82d9a2c2c7f477d8ecae87865
unknown <ajaydevofficial@gmail.com>
2019-03-18 17:55:24 +0530

    App added to Database

--- Document/models.py
+++ Document/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class document(models.Model):
+    name = models.CharField(max_length=200)
+    doc = models.FileField()
+
+    def __init__(self):
+        return self.name
