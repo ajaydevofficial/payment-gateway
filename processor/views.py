6e67d5d18eae3d8eba570241397b65d8e731b61b
unknown <ajaydevofficial@gmail.com>
2019-03-19 21:30:28 +0530

    Payment Gateway connected

--- processor/views.py
+++ processor/views.py
@@ -25,16 +25,17 @@ def home_page(request):
         'CHANNEL_ID' : 'WEB',
         'WEBSITE' : 'WEBSTAGING',
         'INDUSTRY_TYPE_ID' : 'Retail',
-        #'CALLBACK_URL' : 'http://127.0.0.1:8000',
+        'CALLBACK_URL' : 'http://127.0.0.1:1234/success',
 
     }
 
     if request.method=='POST':
         data_dict['CHECKSUMHASH'] = generate_checksum(data_dict,MERCHANT_KEY)
         param_dict = data_dict
-        context['paytm_code']="<h1 hidden>Merchant Check Out Page</h1></br><form method='post' action='https://pguat.paytm.com/oltp-web/processTransaction' name='f1'>"
+        paytmURL = "https://securegw-stage.paytm.in/theia/processTransaction"
+        context['paytm_code']="<h1 hidden>Merchant Check Out Page</h1></br><form method='post' action='"+paytmURL+"'name='f1'>"
         for key in param_dict:
-            context['paytm_code']+="<input type='hidden' name="'+key.strip()+'"value="'+param_dict[key].strip()+'">"
+            context['paytm_code']+="<input type='hidden' name='" + key.strip() + "'value=" + param_dict[key].strip()+">"
         context['paytm_code']+="<script type='text/javascript'>"
         context['paytm_code']+="document.f1.submit();"
         context['paytm_code']+='</script>'
