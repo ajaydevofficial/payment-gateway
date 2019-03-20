79e2edf1be3fe9f3fb7827a5926ea7fd73e426fb
unknown <ajaydevofficial@gmail.com>
2019-03-20 16:22:03 +0530

    Response Completed

--- processor/views.py
+++ processor/views.py
@@ -7,6 +7,7 @@ import requests
 import base64
 import json
 import cgi
+from os import environ
 
 def home_page(request):
     context = {}
@@ -21,16 +22,15 @@ def home_page(request):
     data_dict = {
 
         'MID' :'KLUIOi74399454829212', #use your test or original MID from paytm buisness account
-        'ORDER_ID' : str(order_id.objects.all().values('order_id')[0]['order_id']),
+        'ORDER_ID' : str(order_id.objects.all().values('order_id')[0]['order_id']+1),
         'CUST_ID' : 'Customer_ID', #use different ID for different customers
         'TXN_AMOUNT' : '1.00', #change value accordingly
         'CHANNEL_ID' : 'WEB',
         'WEBSITE' : 'WEBSTAGING',
         'INDUSTRY_TYPE_ID' : 'Retail',
-        'CALLBACK_URL' : 'http://127.0.0.1:1234/success/',
+        'CALLBACK_URL' : 'http://127.0.0.1:8000/response/',
 
     }
-
     if request.method=='POST':
         data_dict['CHECKSUMHASH'] = generate_checksum(data_dict,MERCHANT_KEY)
         param_dict = data_dict
@@ -43,37 +43,49 @@ def home_page(request):
         context['paytm_code']+="document.f1.submit();"
         context['paytm_code']+='</script>'
         context['paytm_code']+='</form>'
+        order_id.objects.update()
 
+        request.session['MID'] = data_dict['MID']
+        request.session['ORDER_ID'] = data_dict['ORDER_ID']
+        request.session['CUST_ID'] = data_dict['CUST_ID']
+        request.session['TXN_AMOUNT'] = data_dict['TXN_AMOUNT']
+        request.session['CHANNEL_ID'] = data_dict['CHANNEL_ID']
+        request.session['WEBSITE'] = data_dict['WEBSITE']
+        request.session['INDUSTRY_TYPE_ID'] = data_dict['INDUSTRY_TYPE_ID']
+        request.session['CALLBACK_URL'] = data_dict['CALLBACK_URL']
+        request.session['CHECKSUMHASH'] = data_dict['CHECKSUMHASH']
 
     return render(request,"index.html",context)
 
 @csrf_exempt
-def success_page(request):
-    context = {}
+def response_page(request):
+    context={}
+    print("Content-type: text/html\n")
+    if request.method=='POST':
+        print(request.POST)
     MERCHANT_KEY = 'jKc6NjVk0T1eZ0Bg'
 
-    form = cgi.FieldStorage()
-    respons_dict = {}
-    print(form)
-
-    for i in form.keys():
-     respons_dict[i]=form[i].value
-     if i=='CHECKSUMHASH':
-        checksum = form[i].value
+    respons_dict = request.POST.copy()
+    checksum = request.POST['CHECKSUMHASH']
 
     if 'GATEWAYNAME' in respons_dict:
     	if respons_dict['GATEWAYNAME'] == 'WALLET':
     		respons_dict['BANKNAME'] = 'null';
 
-    verify = 1#verify_checksum(respons_dict, MERCHANT_KEY, checksum)
-    print(verify)
+    verify = verify_checksum(respons_dict, MERCHANT_KEY, checksum)
 
     if verify:
     	if respons_dict['RESPCODE'] == '01':
-    		print("order successful")
+            context = {
+                'ORDER_ID':request.POST['ORDERID'],
+                'TXN_AMOUNT':request.POST['TXNAMOUNT']
+            }
+            return render(request,"success.html",context)
+
     	else:
-    		print("order unsuccessful because"+respons_dict['RESPMSG'])
+
+    		return render(request,"unsuccess.html",context)
     else:
-    	print("order unsuccessful because"+respons_dict['RESPMSG'])
+    	return render(request,"unsuccess.html",context)
 
     return render(request,"success.html",context)
