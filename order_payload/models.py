a2243947939c4e6bb37fe54365af2e5a416c9cb3
unknown <ajaydevofficial@gmail.com>
2019-03-21 23:17:28 +0530

    Order ID app modified

--- order_payload/models.py
+++ order_payload/models.py
@@ -2,7 +2,8 @@ from django.db import models
 
 # Create your models here.
 class order_id(models.Model):
+    name = models.CharField(default='',max_length=200)
     order_id = models.IntegerField(default=1759)
 
     def __str__(self):
-        return 'ORDER_ID'
+        return 'ID'
